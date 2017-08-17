0f12d4bf4ffc99f9e25ea6917c4e399d48ec0ce3
Swapnilbansal03 <31107500+Swapnilbansal03@users.noreply.github.com>
2017-08-18 00:01:17 +0530

    no message

--- Core_Java/src/com/swapnil/variables/Hello.java
+++ Core_Java/src/com/swapnil/variables/Hello.java
@@ -3,7 +3,7 @@ package com.swapnil.variables;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello hello");
+		System.out.println("Hello hello 2");
 
 	}
 
